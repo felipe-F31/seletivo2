232de8af06281411b7b307e3984f352b38f5d5fa
felipe-F31 <felipeferreira_13@hotmail.com>
2024-03-04 18:52:05 -0300

    initial

---
+++ .env.local.example.php
@@ -0,0 +1,11 @@
+
+<?php
+
+return array(
+    'db_host' => '127.0.0.1',
+    'db_port' => 5432,
+    'db_name' => 'postgres', # nome do seu banco de dados local
+    'db_connection' => 'pgsql',
+    'db_user' => 'postgres', # nome do seu usuario local
+    'db_pass' => 'ferreira' # senha do seu usuario local
+);
---
+++ app/services/NotasFormatar.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Services;
+
+Class NotasFormatar {
+    public function formataNotasPeriodos($notas, $criterio_avaliativo)
+    {
+        if (!empty($notas) && !empty($criterio_avaliativo)) {
+            foreach ($notas as $nota) {
+                $nota->valor_nota = $this->arredondaNota($nota->valor_nota, $criterio_avaliativo->arredondamento_id);
+            }
+        }
+
+        return $notas;
+    }
+
+    public function calculaNotaFinal($notas, $disciplinas, $criterio_avaliativo)
+    {
+        if (!empty($notas) && !empty($disciplinas) && !empty($criterio_avaliativo)) {
+            $array_notas = [];
+
+            foreach ($disciplinas as $disciplina) {
+                foreach ($notas as $nota) {
+                    if ($nota->disciplina_id == $disciplina->disciplina_id) {
+                        $array_notas[] = [
+                            'valor_nota' => $nota->valor_nota,
+                            'disciplina_id' => $nota->disciplina_id
+                        ];
+                    }
+                }
+            }
+
+            return $this->{"calculo$criterio_avaliativo->calculo_id"}($array_notas, $criterio_avaliativo->arredondamento_id);
+        }
+    }
+
+   function calculo1($array_notas, $arredondamento_id) {
+        $array_notas_finais = [];
+        
+        foreach ($array_notas as $nota) {
+            $disciplina_id = $nota['disciplina_id'];
+    
+            if (!isset($array_notas_finais[$disciplina_id])) {
+                $array_notas_finais[$disciplina_id] = [
+                    'disciplina_id' => $disciplina_id,
+                    'soma' => 0,
+                    'contagem' => 0
+                ];
+            }
+    
+            $array_notas_finais[$disciplina_id]['soma'] += $nota['valor_nota'];
+            $array_notas_finais[$disciplina_id]['contagem']++;
+            if ($array_notas_finais[$disciplina_id]['contagem'] == 3 || $array_notas_finais[$disciplina_id]['contagem'] == 4) {
+            $array_notas_finais[$disciplina_id]['soma'] += $nota['valor_nota'];
+            }
+        }
+
+        foreach ($array_notas_finais as &$nota_final) {
+            $nota_final['valor_nota'] = $this->arredondaNota($nota_final['soma'] / 6, $arredondamento_id);
+        }
+    
+        return $array_notas_finais;
+    }
+
+
+    function calculo2($array_notas, $arredondamento_id) {
+        $array_notas_finais = [];
+    
+        foreach ($array_notas as $nota) {
+            $disciplina_id = $nota['disciplina_id'];
+    
+            if (!isset($array_notas_finais[$disciplina_id])) {
+                $array_notas_finais[$disciplina_id] = [
+                    'disciplina_id' => $disciplina_id,
+                    'soma' => 0
+                ];
+            }
+    
+            $array_notas_finais[$disciplina_id]['soma'] += $nota['valor_nota'];
+        }
+
+        foreach ($array_notas_finais as &$nota_final) {
+            $nota_final['valor_nota'] = $this->arredondaNota($nota_final['soma'], $arredondamento_id);
+        }
+        
+        return $array_notas_finais;
+    }
+
+    protected function arredondaNota($nota, $arredondamento_id)
+    {
+        if (!is_null($arredondamento_id) && !is_null($nota)) {
+            return $this->{"arredondamento$arredondamento_id"}($nota);
+        }
+
+        return $nota;
+    }
+
+    protected function arredondamento1($nota_final)
+    {
+         
+    $parte_inteira = (int)$nota_final;
+
+    $parte_decimal = $nota_final - $parte_inteira;
+
+    if ($parte_decimal >= 0.7) {
+        return $parte_inteira + 1;
+    } else {
+        return $parte_inteira;
+    }
+    }
+
+    protected function arredondamento2($valor_nota)
+    {
+        $valor_nota_arredondada = round($valor_nota);
+        
+        return $valor_nota_arredondada;
+    }
+}
